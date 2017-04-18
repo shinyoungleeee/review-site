e0196e6b4206ff6a3a89a5c9b73235b98f94c2cc
ericsharma <sharma.e@husky.neu.edu>
2017-04-18 16:15:42 -0400

    fix more hound violations

--- spec/features/02_user_signs_in_spec.rb
+++ spec/features/02_user_signs_in_spec.rb
@@ -1,28 +1,28 @@
 require 'rails_helper'
 
 feature 'User signs in' do
- scenario 'user signs in successfully' do
-   create(:user)
+  scenario 'user signs in successfully' do
+    create(:user)
 
-   visit new_user_session_path
+    visit new_user_session_path
 
-   fill_in 'Email', with: 'frank@thetank.com'
-   fill_in 'Password', with: 'password123'
-   click_on 'Log in'
+    fill_in 'Email', with: 'frank@thetank.com'
+    fill_in 'Password', with: 'password123'
+    click_on 'Log in'
 
-   expect(page).to have_content 'Signed in successfully.'
- end
+    expect(page).to have_content 'Signed in successfully.'
+  end
 
- scenario 'user signs in with invalid info' do
-   visit new_user_session_path
-   click_on 'Log in'
-   expect(page).to have_content 'Invalid Email or password.'
+  scenario 'user signs in with invalid info' do
+    visit new_user_session_path
+    click_on 'Log in'
+    expect(page).to have_content 'Invalid Email or password.'
 
-   create(:user)
+    create(:user)
 
-   visit new_user_session_path
-   fill_in 'Email', with: 'frank@thetank.com'
-   click_on 'Log in'
-   expect(page).to have_content 'Invalid Email or password.'
- end
+    visit new_user_session_path
+    fill_in 'Email', with: 'frank@thetank.com'
+    click_on 'Log in'
+    expect(page).to have_content 'Invalid Email or password.'
+  end
 end
--- spec/features/03_user_signs_out_spec.rb
+++ spec/features/03_user_signs_out_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 feature 'User signs out' do
- scenario 'user signs out successfully' do
-   create(:user)
+  scenario 'user signs out successfully' do
+    create(:user)
 
-   visit new_user_session_path
+    visit new_user_session_path
 
-   fill_in 'Email', with: 'frank@thetank.com'
-   fill_in 'Password', with: 'password123'
-   click_on 'Log in'
-   click_on 'Sign out'
+    fill_in 'Email', with: 'frank@thetank.com'
+    fill_in 'Password', with: 'password123'
+    click_on 'Log in'
+    click_on 'Sign out'
 
-   expect(page).to have_content 'Signed out successfully.'
- end
+    expect(page).to have_content 'Signed out successfully.'
+  end
 end
