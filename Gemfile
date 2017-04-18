dac9d0451a8848a28bdbfd00a3de4565c09478ce
ericsharma <sharma.e@husky.neu.edu>
2017-04-18 15:21:10 -0400

    pass User model tests

--- Gemfile
+++ Gemfile
@@ -21,7 +21,7 @@ group :development, :test do
   gem 'launchy', require: false
   gem 'pry-rails'
   gem 'rspec-rails', '~> 3.5'
-  gem 'shoulda-matchers', require: false
+  gem 'shoulda-matchers', '~> 3.1'
   gem 'valid_attribute'
   gem 'coveralls', require: false
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :username, presence: true
+  validates :username, uniqueness: true
   validates :email, presence: true
   validates :email, uniqueness: true
   validates :encrypted_password, presence: true
