24473aae27bf6279e439d12c0845f9a4e6f66606
Shinyoung Lee <shinyounglee.315@gmail.com>
2017-04-19 15:03:33 -0400

    add test for grid layout

--- spec/features/05_user_sees_memes_index_page_spec.rb
+++ spec/features/05_user_sees_memes_index_page_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 feature 'user navigates to memes index page' do
   scenario 'sees a list of memes and link for new meme' do
-
     aliens_url = 'http://i3.kym-cdn.com/photos/images/newsfeed/000/158/326/9148130.jpg'
     godfather_url = 'http://i2.kym-cdn.com/photos/images/newsfeed/000/174/143/finding-nemo-blanket.jpeg'
 
@@ -15,5 +14,13 @@ feature 'user navigates to memes index page' do
     expect(page).to have_css("img[src*='http://i3.kym-cdn.com/photos/images/newsfeed/000/158/326/9148130.jpg']")
     expect(page).to have_link 'Ancient Aliens'
     expect(page).to have_link 'Baby Godfather'
+  end
+  scenario 'sees a maximum of 3 memes per row' do
+    4.times { create(:meme) }
+
+    visit memes_path
+
+    expect(page.first('.text-center')).to have_css('img', count: 3)
+    expect(page.all('.text-center')[1]).to have_css('img', count: 1)
   end
 end
