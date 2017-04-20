d0f3db8a22c4f360dc76cdbff0a76e79e6043959
Benjamin Gorbach <bigorbach@gmail.com>
2017-04-20 14:38:10 -0400

    fix hound violations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,10 +1,9 @@
 class Review < ApplicationRecord
   validates :rating, presence: true
-  validates :rating, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 10}
+  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
   validates :votes, presence: true
   validates :votes, numericality: true
 
   belongs_to :user
   belongs_to :meme
-
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -7,5 +7,5 @@ RSpec.describe Review, type: :model do
   it { should validate_numericality_of(:votes) }
   it { should belong_to(:user) }
   it { should belong_to(:meme) }
-  it {should validate_numericality_of(:rating).is_greater_than_or_equal_to(1).is_less_than_or_equal_to(10)}
+  it { should validate_numericality_of(:rating).is_greater_than_or_equal_to(1).is_less_than_or_equal_to(10) }
 end
