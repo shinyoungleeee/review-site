6b4a5cc129ebed8e451c5070906cb3e14d81c76e
Benjamin Gorbach <bigorbach@gmail.com>
2017-04-20 11:22:46 -0400

    add testing and features for destroy

---
+++ spec/features/09_user_deletes_meme_spec.rb
@@ -0,0 +1,16 @@
+feature 'users can delete memes' do
+  scenario 'users deletes meme successfully' do
+    aliens_url = 'http://i3.kym-cdn.com/photos/images/newsfeed/000/158/326/9148130.jpg'
+    ancient_aliens = Meme.create(name: 'Ancient Aliens', image_url: aliens_url, description: 'therefore aliens.')
+
+    visit meme_path(ancient_aliens)
+    expect(page).to have_content 'Delete Meme'
+
+
+
+    expect { click_link 'Delete Meme' }.to change(Meme, :count).by(-1)
+
+    expect(page).to have_content 'Meme deleted successfully'
+
+  end
+end
