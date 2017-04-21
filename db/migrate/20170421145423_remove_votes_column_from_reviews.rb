4171d01267fa22f71591874b9e505777c495057d
ericsharma <sharma.e@husky.neu.edu>
2017-04-21 14:36:51 -0400

    resolve another hound violation

--- db/migrate/20170421145423_remove_votes_column_from_reviews.rb
+++ db/migrate/20170421145423_remove_votes_column_from_reviews.rb
@@ -2,7 +2,7 @@ class RemoveVotesColumnFromReviews < ActiveRecord::Migration[5.0]
   def up
     remove_column :reviews, :votes
   end
-  
+
   def down
     add_column :reviews, :votes, :integer, default: 0
   end
