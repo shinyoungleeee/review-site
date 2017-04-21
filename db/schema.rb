ae7cc91781c338787f27026fb5b4327b835826dc
ericsharma <sharma.e@husky.neu.edu>
2017-04-21 10:56:57 -0400

    remove votes column from reviews table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170420170112) do
+ActiveRecord::Schema.define(version: 20170421145423) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,13 +25,12 @@ ActiveRecord::Schema.define(version: 20170420170112) do
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.integer  "rating",                 null: false
-    t.integer  "votes",      default: 0
+    t.integer  "rating",     null: false
     t.string   "body"
     t.integer  "meme_id"
     t.integer  "user_id"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["meme_id"], name: "index_reviews_on_meme_id", using: :btree
     t.index ["user_id"], name: "index_reviews_on_user_id", using: :btree
   end
