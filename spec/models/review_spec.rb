3b8497da2b7b601b6e0f9d936291d08eb7311d6e
Shinyoung Lee <shinyounglee.315@gmail.com>
2017-04-26 15:41:02 -0400

    improve tests

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -7,4 +7,50 @@ RSpec.describe Review, type: :model do
   it { should belong_to(:meme) }
   it { should have_many(:review_votes) }
   it { should validate_numericality_of(:rating).is_greater_than_or_equal_to(1).is_less_than_or_equal_to(10) }
+
+  describe '#upvotes' do
+    it 'should return the number of upvotes of this review' do
+      review = create(:review)
+      10.times { create(:review_vote, review: review, upvote: true) }
+
+      expect(review.upvotes).to eq(10)
+    end
+  end
+
+  describe '#downvotes' do
+    it 'should return the number of downvotes of this review' do
+      review = create(:review)
+      10.times { create(:review_vote, review: review, downvote: true) }
+
+      expect(review.downvotes).to eq(10)
+    end
+  end
+
+  describe '#vote_count' do
+    it 'should return the number of upvotes minus the number of downvotes of this review' do
+      review = create(:review)
+      10.times { create(:review_vote, review: review, upvote: true) }
+      5.times { create(:review_vote, review: review, downvote: true) }
+
+      expect(review.downvotes).to eq(5)
+
+      10.times { create(:review_vote, review: review, downvote: true) }
+
+      expect(review.downvotes).to eq(-5)
+    end
+  end
+
+  describe '#did_user_vote?' do
+    it 'should take a single argument of a user object' do
+      review = create(:review)
+      10.times { create(:review_vote, review: review, upvote: true) }
+      5.times { create(:review_vote, review: review, downvote: true) }
+
+      expect(review.downvotes).to eq(5)
+
+      10.times { create(:review_vote, review: review, downvote: true) }
+
+      expect(review.downvotes).to eq(-5)
+    end
+  end
 end
