68ff2e91698a7402b29c8090010e6ca4d85abd99
Shinyoung Lee <shinyounglee.315@gmail.com>
2017-04-28 11:42:47 -0400

    fix review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ApplicationRecord
   validates :rating, presence: true
   validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
-  validates :user, uniqueness: { scope: :rating }
+  validates :user, uniqueness: { scope: :meme }
 
   belongs_to :user
   belongs_to :meme
